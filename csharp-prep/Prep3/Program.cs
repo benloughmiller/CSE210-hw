5f8719816266941236de7d2abbed786c532c1288
BenLoughmiller <bloughmiller@byui.edu>
2024-01-18 11:15:39 -0700

    Completed Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int guess = 0;
+        Random random_number = new Random();
+        int magic_number = random_number.Next(1, 100);
+
+        while (guess != magic_number) {
+            Console.WriteLine("Please guess a number");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess > magic_number){
+                Console.WriteLine("Your guess is too high.");
+            }
+            else if (guess < magic_number){
+                Console.WriteLine("Your guess is too Low.");
+            }
+            else {
+                Console.WriteLine("You got it!");
+            }
+            
+        } 
+        
     }
 }
