f27ffbcac7d436019551ce49e36ffc109a93e5d5
BenLoughmiller <bloughmiller@byui.edu>
2024-01-18 17:32:03 -0700

    Completed Prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> number_list = new List<int>();
+        Console.WriteLine("Enter a list of numbers. Type 0 when finished.");
+        int user_input = -1;
+
+        while (user_input != 0) {
+            Console.WriteLine("Please enter a number: ");
+            user_input = int.Parse(Console.ReadLine());
+
+            if (user_input != 0) {
+                number_list.Add(user_input);
+            }
+        }
+
+        int sum = 0;
+        foreach (int num in number_list)
+        {
+            sum += num;
+        }
+        Console.WriteLine($"Sum: {sum}");
+
+        float average = (float)sum / number_list.Count;
+        Console.WriteLine($"Average: {average}");
+
+        int max = 0;
+        foreach (int num in number_list)
+        {
+            if (num > max)
+            {
+                max = num;
+            }
+        }
+        Console.WriteLine($"Maximum: {max}");
     }
 }
