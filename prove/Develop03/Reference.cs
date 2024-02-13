b6a9c8a1f61913b7fe0fb70451dab20cd5ef17b6
BenLoughmiller <bloughmiller@byui.edu>
2024-02-13 10:50:29 -0700

    Filled Reference Class

--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -1,3 +1,20 @@
-//Stores the references for the program.
-using System;
+class Reference {
+    private List<string> verses;
+    private List<string> refs;
+    Random _random = new Random();
 
+    public Reference() {
+        verses = new List<string>() {
+            //Verses 2b added
+        };
+
+        refs = new List<string>() {
+            //References 2b added
+        };
+    }
+
+    public (string Reference, string Verse) GetNextVerse() {
+        int _randomVerse = _random.Next(verses.Count);
+        return (refs[_randomVerse], verses[_randomVerse]);
+    }
+}
