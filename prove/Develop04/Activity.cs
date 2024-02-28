d78e875e282457c598906086440b7d724f1c8783
BenLoughmiller <bloughmiller@byui.edu>
2024-02-27 23:48:23 -0700

    Added Base Files

---
+++ prove/Develop04/Activity.cs
@@ -0,0 +1,20 @@
+class Activity {
+private string _activityName;
+private string _description;
+    public Activity(string name, string description) {
+        _activityName = name;
+        _description = description;
+    }
+    public void ActivityStart() {
+
+    }
+    public void ActivityEnd() {
+        
+    }
+    public void CountdownTimer(int seconds) {
+
+    }
+    public void TimeDelay(int seconds) {
+
+    }
+}
---
+++ prove/Develop04/BreathingActivity.cs
@@ -0,0 +1,11 @@
+class BreathingActivity {
+    public BreathingActivity(){
+
+    }
+
+public void RunBreathing() {
+
+}
+public void BreathingInOut() {
+}
+}
