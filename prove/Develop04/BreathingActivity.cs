d245a4e669ed04c08f6ee22f6a16b687b38d3958
benloughmiller <bloughmiller@byui.edu>
2024-03-01 22:18:16 -0700

    Working on breathing

--- prove/Develop04/BreathingActivity.cs
+++ prove/Develop04/BreathingActivity.cs
@@ -1,15 +1,34 @@
 
-class BreathingActivity : Activity {
-    public BreathingActivity() : base("Breathing", "This activity will help you relax by walking your through breathing in and out slowly. Clear your mind and focus on your breathing.") {
+using System.Runtime.CompilerServices;
 
+class BreathingActivity : Activity {
+
+    public BreathingActivity() : base("Breathing", "This activity will help you relax by walking your through breathing in and out slowly. Clear your mind and focus on your breathing.") {
+    
     }
     public void RunBreathing() {
         ActivityStart();
-        int _duration = GetDuration();
+        int _activityTime = GetDuration();
         SpinnyAnimation(500);
         CountdownTimer(5);
+        BreathingInOut(_activityTime);
         ActivityEnd();
     }
-    public void BreathingInOut() {
+    public void BreathingInOut(int activityTime) {
+        Console.Clear();
+        int _remainingTime = activityTime;
+        while (_remainingTime > 0) {
+            Console.WriteLine("Breathe in...");
+
+            if (_remainingTime >= 5) {
+                CountdownTimer(5);
+                _remainingTime -= 5;
+            }
+            else {
+                CountdownTimer(_remainingTime);
+                _remainingTime -= _remainingTime;  
+            }
+
+        }
     }
 }
