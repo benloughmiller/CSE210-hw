7dd13fcc28ae4f62ea032e7b34f8949394f7710d
benloughmiller <bloughmiller@byui.edu>
2024-04-03 00:32:29 -0600

    That should be about done (hopefully)

--- final/FinalProject/ActivityStatistics.cs
+++ final/FinalProject/ActivityStatistics.cs
@@ -1,12 +1,8 @@
 class ActivityStatistics {
     private List<Activity> _activityList;
-
     private List<Activity> _coinList;
-
     private List<Activity> _spinnerList;
-
     private List<Activity> _diceList;
-
     private List<Activity> _cardList;
     public ActivityStatistics(List<Activity> activityList) {
         _activityList = activityList;
@@ -40,7 +36,7 @@ class ActivityStatistics {
         Console.Write("\nWhich activity would you like to see statistics for: ");
         string statisticsChoice = Console.ReadLine();
 
-        while (statisticsChoice != "1" && statisticsChoice != "2" && statisticsChoice != "3") {
+        while (statisticsChoice != "1" && statisticsChoice != "2" && statisticsChoice != "3" && statisticsChoice != "4") {
             Console.Write("Incorrect Choice. Please enter a valid entry: ");
             statisticsChoice = Console.ReadLine();
         }
@@ -60,21 +56,21 @@ class ActivityStatistics {
             RunStatistics(_cardList);
         }
     }
-    public void RunStatistics(List<Activity> selectedList) {
+    private void RunStatistics(List<Activity> selectedList) {
         DisplayAverage(selectedList);
-        DisplaySums(selectedList);
+        DisplaySum(selectedList);
         DisplayMostCommon(selectedList);
         Console.WriteLine("Press 'Enter' to return to the menu: ");
         Console.ReadLine();
     }
-    public void DisplayAverage(List<Activity> selectedList) {
+    private void DisplayAverage(List<Activity> selectedList) {
         double subtotal = 0;
         foreach (Activity activity in selectedList) {
             subtotal += activity.GetNumericalResult();
         }
         if (subtotal > 0) {
             double average = subtotal / selectedList.Count;
-            Console.WriteLine($"The average result of this activity is: {average:F2}");
+            Console.WriteLine($"\nThe average result of this activity is: {average:F2}");
         }
         else {
             Console.Write("\nNo activities found for calculating the average.");
@@ -82,15 +78,73 @@ class ActivityStatistics {
         }
 
     }
-    public void DisplaySums(List<Activity> selectedList) {
-        //To Be Added
-        //Show the sum of all results
+    private void DisplaySum(List<Activity> selectedList) {
+        int total = 0;
+        foreach (Activity activity in selectedList) {
+            total += activity.GetNumericalResult();
+        }
+        Console.WriteLine($"\nThe total of all results for this activity is: {total}");
     }
-    public void DisplayMostCommon(List<Activity> selectedList) {
-        //To Be Added
-        //Coin: show most flipped side
-        //Spinner: Show most spun side
-        //Dice: Show most rolled side
-        //Card: Show most drawn card, most drawn number, most drawn suit
+    private void DisplayMostCommon(List<Activity> selectedList) {
+        //Reference: https://www.geeksforgeeks.org/c-sharp-dictionary-containskey-method/
+        Dictionary<string, int> cardResults = new Dictionary<string, int>();
+        Dictionary<string, int> suitResults = new Dictionary<string, int>();
+        Dictionary<string, int> rankResults = new Dictionary<string, int>();
+        Dictionary<string, int> countResults = new Dictionary<string, int>();
+
+        foreach (Activity activity in selectedList) {
+        if (activity is CardActivity) {
+                CardActivity cardActivity = (CardActivity)activity;
+                string suit = cardActivity.GetSuit();
+                string rank = cardActivity.GetRank();
+                string card = $"{rank} of {suit}";
+                if (suitResults.ContainsKey(suit)) {
+                    suitResults[suit]++;
+                }
+                else {
+                    suitResults[suit] = 1;
+                }
+                if (rankResults.ContainsKey(rank)) {
+                    rankResults[rank]++;
+                }
+                else {
+                    rankResults[rank] = 1;
+                }
+                if (cardResults.ContainsKey(card)) {
+                    cardResults[card]++;
+                }
+                else {
+                    cardResults[card] = 1;
+                } 
+            }
+            else {
+                string result = activity.GetResult();
+                if (countResults.ContainsKey(result)) {
+                    countResults[result]++;
+                } 
+                else {
+                    countResults[result] = 1;
+                }
+            }
+        }
+        Console.WriteLine("Sorted Outcomes:");
+        foreach (var value in countResults.OrderByDescending(pair => pair.Value)) {
+            Console.WriteLine($"{value.Key}: {value.Value}");
+        }
+        if (cardResults.Keys.Any(key => key.Contains("of"))) {
+            Console.WriteLine("\nSorted Suits:");
+            foreach (var value in suitResults.OrderByDescending(pair => pair.Value)) {
+                Console.WriteLine($"{value.Key}: {value.Value}");
+            }
+            Console.WriteLine("\nSorted Ranks:");
+            foreach (var value in rankResults.OrderByDescending(pair => pair.Value)) {
+                Console.WriteLine($"{value.Key}: {value.Value}");
+            }
+            Console.WriteLine("\nRanked Cards:");
+            foreach (var value in cardResults.OrderByDescending(pair => pair.Value)) {
+                Console.WriteLine($"{value.Key}: {value.Value}");
+            }
+        }
+
     }
 }
